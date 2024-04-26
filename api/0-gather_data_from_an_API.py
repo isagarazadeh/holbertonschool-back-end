20b627bf91bb5f309eff278521c21260eb778c92
root <garazadeisa625@gmail.com>
2024-04-26 12:01:32 +0000

    task1

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -6,24 +6,19 @@ import requests
 def get_employee_todo_progress(employee_id):
 
     api_url = "https://api.example.com/employees/{}/tasks".format(employee_id)
-
     response = requests.get(api_url)
     response_data = response.json()
-
-
     employee_name = response_data.get("employee_name")
     total_tasks = len(response_data.get("tasks"))
-    done_tasks = sum(task.get("status") == "Done"
-            for task in response_data.get("tasks"))
-
-
+    done_tasks = sum(task.get("status") == "Done")
     print(f"Employee {employee_name} is done with tasks
-            ({done_tasks}/{total_tasks}):")
+          ({done_tasks}/{total_tasks}): ")
+
     for task in response_data.get("tasks"):
         if task.get("status") == "Done":
             print(f"\t{task.get('title')}")
 
+
 if __name__ == "__main__":
     employee_id = int(input("Enter the employee ID: "))
     get_employee_todo_progress(employee_id)
-
