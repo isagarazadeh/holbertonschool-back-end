b86d2c8c28ed6a13e0daaaa2dfa0872faeba2a74
root <garazadeisa625@gmail.com>
2024-04-28 09:06:03 +0000

    export ot csv

---
+++ api/1-export_to_CSV.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""API Module"""
+import requests
+import sys
+
+if len(sys.argv) == 2:
+    url = "https://jsonplaceholder.typicode.com/"
+    usr_id = sys.argv[1]
+    todos = requests.get(f"{url}users/{usr_id}/todos").json()
+    res1 = requests.get(f"{url}users/{usr_id}").json()
+    string = ""
+    usr_name = res1["username"]
+    for i in todos:
+        completed = "True" if i['completed'] else "False"
+            title = i['title']
+            csvfile.write(f'"{id}","{name}","{completed}","{title}"\n')
+
+    with open(f"{usr_id}.csv", "w") as csv:
+        csv.write(string)
